6322385ac8ee5d9bc14a7f58f6c8c3783d00929e
yidafu <tiaozuwushi@qq.com>
2022-06-05 21:51:32 +0800

    fix: make all build fail

--- src/vgastr.h
+++ src/vgastr.h
@@ -1,16 +1,2 @@
 void _strwrite(char* string);
 void printf(char* fmt, ...);
-
-void _strwrite(char* string) {
-  char* p_strdst = (char *)(0xb8000);
-  while (*string)
-  {
-    *p_strdst = *string++;
-    *p_strdst += 2;
-  }
-  return;
-}
-
-void printf(char* fmt, ...) {
-  _strwrite(fmt);
-}
